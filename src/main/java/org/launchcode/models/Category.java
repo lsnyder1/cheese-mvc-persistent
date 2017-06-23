fcdef2ab4c38615143ae13864b104ad427c70b8c
Lonny Snyder <lonnysnyder81@gmail.com>
2017-06-22 19:24:06 -0500

    fixed port issue with database, and added Category model class

---
+++ src/main/java/org/launchcode/models/Category.java
@@ -0,0 +1,27 @@
+package org.launchcode.models;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+import javax.validation.constraints.NotNull;
+import javax.validation.constraints.Size;
+
+/**
+ * Created by lonny on 6/22/2017.
+ */
+@Entity
+public class Category {
+    @Id
+    @GeneratedValue
+    private int id;
+
+    @NotNull
+    @Size(min=2,max=15)
+    private String name;
+
+    public Category(String name){
+        this.name=name;
+    }
+    public Category(){}
+
+}
