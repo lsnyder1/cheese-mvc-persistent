e116694a24093f5a62eec215be6747bc7c527293
Lonny Snyder <lonnysnyder81@gmail.com>
2017-06-27 17:28:49 -0500

    added index method to menu handler and menu/index.html

--- src/main/java/org/launchcode/controllers/MenuController.java
+++ src/main/java/org/launchcode/controllers/MenuController.java
@@ -1,7 +1,31 @@
 package org.launchcode.controllers;
 
+import groovy.util.logging.Commons;
+import org.launchcode.models.data.CheeseDao;
+import org.launchcode.models.data.MenuDao;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+
 /**
  * Created by lonny on 6/27/2017.
  */
+@Controller
+@RequestMapping(value="menu")
 public class MenuController {
+
+    @Autowired
+    private MenuDao menuDao;
+
+    @Autowired
+    private CheeseDao cheeseDao;
+
+    @RequestMapping(value="")
+    public String index(Model model){
+        model.addAttribute("title","Menus")
+        model.addAttribute("menus",menuDao.findAll());
+        return"menu/index";
+    }
+
 }
