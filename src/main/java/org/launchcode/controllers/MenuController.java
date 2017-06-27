da0ecfe470950bce8239f1197db587975731a5d3
Lonny Snyder <lonnysnyder81@gmail.com>
2017-06-27 17:41:19 -0500

    added menu/add.html

--- src/main/java/org/launchcode/controllers/MenuController.java
+++ src/main/java/org/launchcode/controllers/MenuController.java
@@ -1,12 +1,14 @@
 package org.launchcode.controllers;
 
 import groovy.util.logging.Commons;
+import org.launchcode.models.Menu;
 import org.launchcode.models.data.CheeseDao;
 import org.launchcode.models.data.MenuDao;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 
 /**
  * Created by lonny on 6/27/2017.
@@ -23,9 +25,15 @@ public class MenuController {
 
     @RequestMapping(value="")
     public String index(Model model){
-        model.addAttribute("title","Menus")
+        model.addAttribute("title","Menus");
         model.addAttribute("menus",menuDao.findAll());
         return"menu/index";
     }
-
+    @RequestMapping(value="add",method= RequestMethod.GET)
+    public String displayAddMenuForm(Model model){
+        Menu menu=new Menu();
+        model.addAttribute("title","Add Menu")
+        model.addAttribute("menu",menu);
+        return"menu/add";
+    }
 }
